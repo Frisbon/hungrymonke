4e3a78dfb86f29a203c0ff564d33a7b7135e8090
Sasholo <sashabazhura123@gmail.com>
2025-10-28 05:07:42 +0100

    sorry lol

--- webui/src/components/ConversationList.vue
+++ webui/src/components/ConversationList.vue
@@ -353,7 +353,7 @@ export default {
       this.pollingInterval = setInterval(() => {
         console.log("Polling for new conversations...");
         this.pollingFetcher();
-      }, 10000); //  3000ms = 3 second
+      }, 5000); //  3000ms = 3 second
     },
 
     stopPolling() {
